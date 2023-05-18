7fd351a25b67da7a1ae900966303afad78e4506e
hoangviet-nguyen <viety.nguyen02@gmail.com>
2023-05-18 16:40:02 +0200

    commented Huffman

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,14 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Huffman {
 
     /**
      *
-     * @param root
-     * @param s
-     * @param table
-     * @return
+     * @param root root node of the Huffman tree
+     * @param s builds codePoint String of the characters
+     * @param table Map characters to occurences
+     * @return return a Map with characters and Codepoint
      */
     public static Map<Character, String> printCode(HuffmanNode root, String s, Map<Character, String> table) {
         if (root.left == null && root.right == null) {
@@ -21,6 +22,12 @@ public class Huffman {
         return table;
     }
 
+    /**
+     *
+     * @param table Map codePoint to character
+     * @param code with Huffman encoded String
+     * @return decoded value of the the code
+     */
     public static String decode(Map<String, Character> table, String code) {
         StringBuilder decoded = new StringBuilder();
         String codepoint = "";
@@ -34,5 +41,44 @@ public class Huffman {
         return decoded.toString();
     }
 
+    /**
+     *
+     * @param charfreq Array with frequencies of charactes
+     * @param charArray Array with all distinct characters in text
+     * @return root Huffman Node
+     */
+    public static HuffmanNode treeGenerator(int [] charfreq, char[] charArray) {
+        int n = charArray.length;
+        PriorityQueue<HuffmanNode> q = new PriorityQueue<HuffmanNode>(n, new NodeComparator());
+
+        for (int i = 0; i <n; i++) {
+
+            HuffmanNode hn = new HuffmanNode();
+            hn.c = charArray[i];
+            hn.item = charfreq[i];
+            hn.left = null;
+            hn.right = null;
+            q.add(hn);
+        }
+
+        HuffmanNode root = null;
+
+        while (q.size() > 1) {
+
+            HuffmanNode x = q.peek();
+            q.poll();
+            HuffmanNode y = q.peek();
+            q.poll();
+            HuffmanNode f = new HuffmanNode();
+            f.item = x.item + y.item;
+            f.c = '-';
+            f.left = x;
+            f.right = y;
+            root = f;
+            q.add(f);
+
+        }
+        return root;
+    }
 
 }
